8772c173c323ffe4774cdf7f48ef1870ac9901f5
Artem Everstov <39259655+martosea@users.noreply.github.com>
2021-12-13 17:26:05 +0300

    Create task_2_5.java

---
+++ src/ru/mirea/task2/task_2_5.java
@@ -0,0 +1,12 @@
+package ru.mirea.task2;
+
+import java.lang.*;
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball ("Red" , 10);
+        Ball b2 = new Ball (4);
+
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
