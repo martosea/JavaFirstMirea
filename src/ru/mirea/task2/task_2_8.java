0a06b3622cbfdb1d8009af080b35239714b8cb43
Artem Everstov <39259655+martosea@users.noreply.github.com>
2021-12-13 17:27:23 +0300

    Create task_2_8.java

---
+++ src/ru/mirea/task2/task_2_8.java
@@ -0,0 +1,12 @@
+package ru.mirea.task2;
+
+import java.lang.*;
+public class TestShape {
+    public static void main(String[] args) {
+        Shape s1 = new Shape ("Rectangle" , "Red");
+        Shape s2 = new Shape ("Circle", "White");
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
