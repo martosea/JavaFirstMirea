7c150d68ff1ec21fd040aabb6c0e1f378b31de43
Artem Everstov <39259655+martosea@users.noreply.github.com>
2021-12-13 17:42:05 +0300

    Create task_4_2.java

---
+++ src/ru/mirea/task4/task_4_1/task_4_2.java
@@ -0,0 +1,9 @@
+package ru.mirea.task4;
+
+import java.lang.*;
+public class TestAuthor {
+    public static void  main(String[] args) {
+        Author a1 = new Author("Grigorii", "Grigorii@gen.net" , 'M');
+        System.out.println(a1);
+    }
+}
