963d987a592ffbdbdc9c7d7d8bb217a888dd89ad
Artem Everstov <39259655+martosea@users.noreply.github.com>
2021-12-13 17:48:55 +0300

    Create task_6_2.java

---
+++ src/ru/mirea/task6/task_6_2.java
@@ -0,0 +1,14 @@
+package ru.mirea.task6.opt1;
+
+import java.lang.*;
+public class TestNameable {
+    public static void main(String[] args) {
+        read(new Planet("Earth"));
+        read(new Car("Volkswagen"));
+        read(new Animal("Dog"));
+    }
+
+    static void read(Nameable n) {
+        n.getName();
+    }
+}
